8f70ec64cdf25136bb1890ddc59b7dbb095eecbb
Jason Zoller <zoller.jason@gmail.com>
2016-12-07 01:47:16 -0600

    added search for questions in admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
             ('Date Information', {'fields' : ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    search_fields = ['question_text']
     list_filter = ['pub_date']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
 
