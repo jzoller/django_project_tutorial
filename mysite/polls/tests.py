bea6d215ba844cc5c07314761dec4a3289d99f71
Jason Zoller <zoller.jason@gmail.com>
2016-12-06 22:19:23 -0600

    added tests for index view and fixed punctuation error in index template

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,10 +1,20 @@
 import datetime
 
+from django.urls import reverse
 from django.utils import timezone
 from django.test import TestCase
 
 from .models import Question
 # Create your tests here.
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given question_text and published on a given number of days
+    offset to now(negative values indicate the past, positive for questions that have yet to be 
+    published, zero for now)
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestoinMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -30,3 +40,53 @@ class QuestoinMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionViewTests(TestCase):
+
+    def test_index_view_wtih_no_questions(self):
+        """
+        If not questions exist, a message should be displayed
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the index page
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+    def test_index_view_with_a_future_question(self):
+        """
+        Questions witha pub_date in the future should not be displayed on index page
+        """
+        create_question(question_text='Future question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        Only past Question should be displayed.
+        """
+        create_question(question_text="Past Question.", days=-30)
+        create_question(question_text="Future Question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+                response.context['latest_question_list'], 
+                ['<Question: Past Question.>'])
+
+    def test_index_view_with_two_psat_questions(self):
+        """
+        Questions index should display multiple questions.
+        """
+        create_question(question_text='Past question 1.', days=-30)
+        create_question(question_text='Past question 2.', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+                response.context['latest_question_list'],
+                ['<Question: Past question 2.>', '<Question: Past question 1.>'])
